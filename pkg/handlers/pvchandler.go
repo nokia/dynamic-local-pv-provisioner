1c7af6505a29149a1a36935f6be59b405f33fee6
Tobik, Balint <balint.tobik@nokia.com>
2019-10-04 16:01:15 +0200

    corrections and fixes

--- pkg/handlers/pvchandler.go
+++ pkg/handlers/pvchandler.go
@@ -9,7 +9,6 @@ import (
 	"fmt"
 	"time"
 	"reflect"
-	"errors"
 
 	"k8s.io/api/core/v1"
 	"k8s.io/client-go/informers"
@@ -55,30 +54,35 @@ func (pvcHandler *PvcHandler) CreateController() cache.Controller {
 
 func (pvcHandler *PvcHandler) pvcAdded(pvc v1.PersistentVolumeClaim) {
 	log.Printf("DEBUG: PVC Added: %+v\n", pvc)
-	if nodeName, ok := pvc.ObjectMeta.Annotations["nodename"]; (pvc.Status.Phase != v1.ClaimBound) && ok && (nodeName == os.Getenv("NODE_NAME")) {
-		log.Println("DEBUG: PVC handle")
-		pvcStorageReq := pvc.Spec.Resources.Requests["storage"]
-		err := pvcHandler.createPVStorage(strconv.FormatInt((&pvcStorageReq).Value(), 10))
-		if err != nil{
-			log.Println("ERROR: PVC-Added failed: " + err.Error())
-		}
-	}else{
+	handlePvc, pvDirPath := shouldPvcBeHandled(pvc, pvcHandler.storagePath)
+	if ! handlePvc {
 		log.Println("DEBUG: pvcAdded - Not my job...")
+		return
 	}
+	pvcHandler.createPVStorage(pvc, pvDirPath)
 }
 
 func (pvcHandler *PvcHandler) pvcChanged(oldPvc v1.PersistentVolumeClaim, newPvc v1.PersistentVolumeClaim) {
 	log.Printf("PVC changed: %+v\n", newPvc)
-	_, oldOk:= oldPvc.Spec.Resources.Requests["storage"];
-	pvcStorageReq, newOk := newPvc.Spec.Resources.Requests["storage"]
-	if (newPvc.Status.Phase != v1.ClaimBound) && !oldOk && newOk{
-		err := pvcHandler.createPVStorage(strconv.FormatInt((&pvcStorageReq).Value(), 10))
-		if err != nil{
-			log.Println("ERROR: PVC-Changed failed: " + err.Error())
-		}
-	}else{
+	handlePvc, pvDirPath := shouldPvcBeHandled(newPvc, pvcHandler.storagePath)
+	if !handlePvc {
 		log.Println("DEBUG: pvcChanged - Not my job...")
+		return
 	}
+	pvcHandler.createPVStorage(newPvc, pvDirPath)
+}
+
+func shouldPvcBeHandled(pvc v1.PersistentVolumeClaim, storagePath string) (bool, string) {
+	if pvc.Status.Phase == v1.ClaimPending {
+		if nodeName, ok := pvc.ObjectMeta.Annotations["nodename"]; ok && (nodeName == os.Getenv("NODE_NAME")) {
+			pvDir := storagePath + "/" + pvc.ObjectMeta.Namespace + "_" + pvc.ObjectMeta.Name
+			if _, err := os.Stat(pvDir); os.IsNotExist(err) {
+				return true, pvDir
+			}
+			log.Println("DEBUG: " + pvDir + " already exists!")
+		}
+	}
+	return false, ""
 }
 
 func runOSCommand(command string) (string, error) {
@@ -92,61 +96,70 @@ func runOSCommand(command string) (string, error) {
 	return output, nil
 }
 
-func (pvcHandler *PvcHandler) createPVStorage(storageRequest string) error{
+func (pvcHandler *PvcHandler) createPVStorage(pvc v1.PersistentVolumeClaim, pvDirPath string) {
 	log.Println("DEBUG: Starting createPVStorage executor...")
-	if storageRequest == ""{
-		return errors.New("Error: Storage request is empty...")
+	pvcStorageReq, ok := pvc.Spec.Resources.Requests["storage"]
+	if !ok {
+		log.Println("ERROR: Storage request is empty!")
+		return
 	}
+	storageRequest := strconv.FormatInt((&pvcStorageReq).Value(), 10)
 	// get xfs_quota last proj_id
 	command := "xfs_quota -x -c report " + pvcHandler.storagePath + " | grep '#'"
 	log.Println(command)
 	output, err := runOSCommand(command)
 	if err != nil {
-		return errors.New("ERROR: Cannot get XFS quota reports: " + err.Error())
+		log.Println("ERROR: Cannot get XFS quota reports, because: " + err.Error())
+		return
 	}
 	lines := strings.Split(output,"\n")
 	lastline := lines[len(lines)-2]
 	projID, err := strconv.Atoi(strings.TrimPrefix(strings.Split(lastline," ")[0],"#"))
 	if err != nil{
-		return errors.New("ERROR: Cannot convert project id from " + lastline + " because:" + err.Error())
+		log.Println("ERROR: Cannot convert project id from " + lastline + " because: " + err.Error())
+		return
 	}
 	// create directory with new projID
 	projID = projID + 1
-	pvDirPath := fmt.Sprintf("%s/%d", pvcHandler.storagePath, projID)
 	err = os.Mkdir(pvDirPath, os.ModePerm)
 	if err != nil {
-		return errors.New("ERROR: Cannot create directory: " + err.Error())
+		log.Println("ERROR: Cannot create directory on host, because: " + err.Error())
+		return
 	}
 	// set xfs_quota limit
 	command = fmt.Sprintf("xfs_quota -x -c 'project -s -p %s %d' %s", pvDirPath, projID, pvcHandler.storagePath)
 	_, err = runOSCommand(command)
 	if err != nil {
-		return errors.New("ERROR: Cannot set xfs_quota project:" + err.Error())
+		log.Println("ERROR: Cannot set xfs_quota project, because: " + err.Error())
+		return
 	}
 	command = fmt.Sprintf("xfs_quota -x -c 'limit -p bhard=%s %d' %s", storageRequest, projID, pvcHandler.storagePath)
 	log.Println(command)
 	_, err = runOSCommand(command)
 	if err != nil {
-		return errors.New("ERROR: Cannot set xfs_quota limit:" + err.Error())
+		log.Println("ERROR: Cannot set xfs_quota limit, because: " + err.Error())
+		return
 	}
 
 	log.Println("DEBUG: Bind Mount... ")
-	// err = mount.MakeMount(pvDirPath)
+	// Bind mounting
 	err = syscall.Mount(pvDirPath, pvDirPath, "none", syscall.MS_BIND, "")
 	if err != nil {
-		return errors.New("Error bind mount directories: " + err.Error())
+		log.Println("ERROR: bind mount directories, because: " + err.Error())
+		return
 	}
 	// Set fstab file
-	file, err := os.OpenFile(fstabPath, os.O_WRONLY|os.O_SYNC, 0755)
+	file, err := os.OpenFile(fstabPath, os.O_APPEND|os.O_WRONLY|os.O_SYNC, 0755)
 	if err != nil {
-		return errors.New("Can't open fstab file: " + fstabPath + " because:" + err.Error() + "\nCannot save mountpoint!")
+		log.Println("ERROR: Cannot open fstab file: " + fstabPath + " because: " + err.Error()+ "\nCannot save mountpoint!")
+		return
 	}
 	defer file.Close()
 	bindMountCommand := fmt.Sprintf("%[1]s %[1]s none bind 0 0", pvDirPath)
 	_,err = file.WriteString(bindMountCommand)
 	if err != nil {
-		return errors.New("Can't modify fstab file: " + fstabPath + " because:" + err.Error() + "\nCannot save mountpoint!")
+		log.Println("ERROR: Cannot modify fstab file: " + fstabPath + " because: " + err.Error()+ "\nCannot save mountpoint!")
+		return
 	}
 	log.Println("DEBUG: createPVStorage executor successfull!")
-	return nil
 }
--- pkg/k8sclient/k8sclient.go
+++ pkg/k8sclient/k8sclient.go
@@ -7,22 +7,12 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/api/resource"
 )
 
 func GetNodeByLabel(label string, kubeClient kubernetes.Interface) (v1.Node, error) {
 	var returnNode v1.Node
 	var maxCapacity int64 = 0
-	// creates the in-cluster config
-	// config, err := rest.InClusterConfig()
-	// if err != nil {
-	// 	return nil,err
-	// }
-	// // creates the clientset
-	// clientset, err := kubernetes.NewForConfig(config)
-	// if err != nil {
-	// 	return nil,err
-	// }
+
 	nodeList, err := kubeClient.CoreV1().Nodes().List(metav1.ListOptions{LabelSelector: label})
 	log.Printf("DEBUG: result node list: %+v\n", nodeList)
 	if err != nil {
@@ -55,17 +45,23 @@ func UpdatePvc(pvc v1.PersistentVolumeClaim, kubeClient kubernetes.Interface) er
 	return nil
 }
 
-func UpdateNodeLVCapacity(nodeName string, lvCapacity int64, kubeClient kubernetes.Interface) error {
+func GetNode(nodeName string, kubeClient kubernetes.Interface) (*v1.Node, error) {
 	node, err := kubeClient.CoreV1().Nodes().Get(nodeName, metav1.GetOptions{})
  	if err != nil {
-		return errors.New("ERROR: Cannot get lv-capacity on node because: " + err.Error())
+		return nil, errors.New("ERROR: Cannot get node (" + nodeName + ") because: " + err.Error())
 	}
-	lvCapQuantity := resource.NewQuantity(lvCapacity, resource.DecimalSI)
-	node.Status.Capacity["lv-capacity"] = *lvCapQuantity
+	return node, nil
+}
 
-	_, err = kubeClient.CoreV1().Nodes().UpdateStatus(node)
+func UpdateNodeStatus(nodeName string, kubeClient kubernetes.Interface, node *v1.Node) error {
+	// node, err := kubeClient.CoreV1().Nodes().Get(nodeName, metav1.GetOptions{})
+ 	// if err != nil {
+	// 	return errors.New("ERROR: Cannot get lv-capacity on node because: " + err.Error())
+	// }
+	// node.Status.Capacity["lv-capacity"] = lvCapQuantity
+	_, err := kubeClient.CoreV1().Nodes().UpdateStatus(node)
 	if err != nil {
-		return errors.New("ERROR: Cannot update lv-capacity on node because: " + err.Error())
+		return errors.New("ERROR: Cannot update node staus of node (" + nodeName + ") because: " + err.Error())
 	}
 	return nil
 }
